9a72b67b2cf568d76c90d8472344fe35f3811dc0
norandex <norilsk.andrei@yandex.ru>
2024-08-25 01:25:49 +0300

    2.1

--- main-service/src/main/java/ru/practicum/controller/publicController/PublicCategoryController.java
+++ main-service/src/main/java/ru/practicum/controller/publicController/PublicCategoryController.java
@@ -2,7 +2,6 @@ package ru.practicum.controller.publicController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.category.CategoryDto;
