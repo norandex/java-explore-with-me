cfddee3b2c8e4544fc583f11301f82ae84589d12
norandex <norilsk.andrei@yandex.ru>
2024-08-27 20:05:31 +0300

    3.1

--- main-service/src/main/java/ru/practicum/util/DatePattern.java
+++ main-service/src/main/java/ru/practicum/util/DatePattern.java
@@ -1,8 +1,10 @@
 package ru.practicum.util;
 
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DatePattern {
     public static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
 
-    private DatePattern() {
-    }
 }
